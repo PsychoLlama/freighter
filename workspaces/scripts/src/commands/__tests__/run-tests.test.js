9dbc17dac5aef81c5dbbd3d61d1ea00602cc72ed
Jesse Gibson <JesseTheGibson@gmail.com>
2018-09-09 19:26:07 -0600

    Skip tests if no workspaces exist
    Objective: test, lint, flow, and ci commands should pass successfully
    out of the box. For both eslint and Jest, they don't like it when a glob
    doesn't match any patterns, and there won't be any patterns until the
    user adds a workspace.
    
    This quits tests early with a warning if no workspaces were found.

--- workspaces/scripts/src/commands/__tests__/run-tests.test.js
+++ workspaces/scripts/src/commands/__tests__/run-tests.test.js
@@ -1,15 +1,19 @@
 // @flow
 import { spawn } from 'promisify-child-process';
 
+import { hasWorkspaces } from '../utils/workspaces';
 import { CONFIG, JEST_PATH } from '../run-tests';
 import { cli } from '../../test-utils';
 
 jest.mock('promisify-child-process');
+jest.mock('../utils/workspaces');
+jest.mock('@freighter/logger');
 
 describe('test', () => {
   beforeEach(() => {
     jest.clearAllMocks();
 
+    (hasWorkspaces: Function).mockResolvedValue(true);
     (spawn: Function).mockResolvedValue({
       stderr: null,
       stdout: null,
@@ -39,5 +43,12 @@ describe('test', () => {
     expect(result).toMatchObject({
       code: 15,
     });
+  });
+
+  it('skips the tests if no workspaces exist', async () => {
+    (hasWorkspaces: Function).mockResolvedValue(false);
+    await cli('test');
+
+    expect(spawn).not.toHaveBeenCalled();
   });
 });
--- workspaces/scripts/src/commands/run-tests.js
+++ workspaces/scripts/src/commands/run-tests.js
@@ -1,6 +1,8 @@
 // @flow
 import { spawn } from 'promisify-child-process';
+import logger from '@freighter/logger';
 
+import { hasWorkspaces } from './utils/workspaces';
 import { command, exit } from './decorator';
 
 export const JEST_PATH = require.resolve('jest/bin/jest');
@@ -18,6 +20,12 @@ type Options = {
 };
 
 export const test = async (cmd: Options) => {
+  const repoPath = process.cwd();
+  if (!(await hasWorkspaces(repoPath))) {
+    logger.warn('No workspaces found. Skipping tests.');
+    return;
+  }
+
   const givenArgs = [];
   if (cmd.watch) {
     givenArgs.push('--watch', '--collectCoverage=false');
